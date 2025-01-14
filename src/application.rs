02d7a54efc81b03a9ca0e7ca80da2e9b23ad10d5
Alexey Sokolov <alexey@asokolov.org>
2025-01-14 02:04:02 +0000

    more menu items

--- src/application.rs
+++ src/application.rs
@@ -64,6 +64,11 @@ mod imp {
 				window.upcast()
 			};
 
+			/*let theme =gtk::IconTheme::for_display(&window.clone().upcast::<gtk::Widget>().display());
+			let mut v = theme.icon_names();
+			v.sort();
+			dbg!(v);*/
+
 			// Ask the window manager/compositor to present the window
 			window.present();
 		}
